cf801852dd56acb64d41e9bf8e5a278771f4e264
codwerk <louisws@outlook.fr>
2021-11-01 14:25:09 +0100

    La boucle
    La boucle est le code PHP qui permet à WordPress d'afficher une série d'article.

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?= get_header(); ?>
 
-Bonjour widdy
+<?php if (have_posts()) : ?>
+    <div class="row">
+      <?php while (have_posts()) : the_post(); ?>
+        <div class="col-md-4">
+          <div class="card">
+            <?php the_post_thumbnail('meduim', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;'])?>
+            <div class="card-body">
+              <h5 class="card-title"><?= the_title(); ?></h5>
+              <h6 class="card-subtitle mb-2 text-muted"><?= the_category() ?></h6>
+              <p class="card-text"><?= the_excerpt(); ?></p >
+              <a href="<?= the_permalink() ?>" class="card-link">Voir Plus</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+        </div>
+
+      <?php endwhile; ?>
+    </div>
+<?php else : ?>
+  <h1>Pas d'articles</h1>
+<?php endif; ?>
 
 <?= get_footer(); ?>
