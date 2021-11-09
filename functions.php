11e9be29476b0973d82da0c58a4d516c52206c49
codwerk <louisws@outlook.fr>
2021-11-10 00:25:57 +0100

    Gérer les menus

--- functions.php
+++ functions.php
@@ -3,14 +3,19 @@
 function montheme_supports() {
   add_theme_support('title-tag'); // title dynamique
   add_theme_support('post-thumbnails');
+  add_theme_support('menus');
+  register_nav_menu('header', 'En tête du menu');
+  register_nav_menu('footer', 'Pied de page');
 }
 
 function montheme_register_assets() {
+  wp_register_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', []);
   wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', []);
   wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js', ['bundle', 'popper'], null, true);
   wp_register_script('bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', [], null, true);
   wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js', [], false, true);
   // []: pas de dependances, false: pas de version, true: dans le footer
+  wp_enqueue_style( 'fontawesome' );
   wp_enqueue_style( 'bootstrap' );
   wp_enqueue_script( 'bootstrap' );
 }
@@ -28,7 +33,25 @@ function montheme_document_title_parts($title) {
     return $title;
 }
 
+function montheme_menu_class($classes) {
+  //var_dump(func_get_args());
+  //die();
+
+  $classes[] = 'nav-item';
+  return $classes;
+}
+
+function montheme_menu_link_class($attrs) {
+  //var_dump(func_get_args());
+  //die();
+
+  $attrs['class'] = 'nav-link';
+  return $attrs;
+}
+
 add_action('after_setup_theme', 'montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 add_filter('document_title_separator', 'montheme_title_separator');
 add_filter('document_title_parts', 'montheme_document_title_parts');
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
--- header.php
+++ header.php
@@ -7,6 +7,33 @@
   <?= wp_head(); ?>
 </head>
 <body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+    <div class="container"> 
+      <a class="navbar-brand text-white" href="#">
+        <i class="fa d-inline fa-lg fa-stop-circle"></i>
+        <b><?= bloginfo('name') ?></b>
+      </a> 
+      <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar4">
+        <span class="navbar-toggler-icon" ></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbar4">
+        <?= wp_nav_menu([
+          'theme_location' => 'header', 
+          'container' => false,
+          'menu_class' => 'navbar-nav ml-auto'
+          ]); ?>
+        <!--
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#">Pricing</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#">FAQ</a> </li>
+          </ul> 
+        -->
+        <a class="btn navbar-btn ml-md-2 btn-light">Contact us</a>
+      </div>
+    </div>
+  </nav>
   <div class="container">
 
   
